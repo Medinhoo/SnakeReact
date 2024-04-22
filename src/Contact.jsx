c573f1483715504e1e16624d67f059270bf26ab0
Mehdi <chihi.medrgmail.com>
2024-04-22 03:41:08 +0200

    stylizing my Contact and rules pages

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,5 +1,6 @@
 export const Contact = () => {
     return (
-        <a href="https://github.com/Medinhoo/" target="_blank">My Github</a>
-    )
+<div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+      <a href="https://github.com/Medinhoo/" target="_blank" className="btn btn-primary">My Github</a>
+    </div>    )
 }
--- src/Rules.jsx
+++ src/Rules.jsx
@@ -1,7 +1,12 @@
 export const Rules = () => {
     return (
-        <div>
-            The "head" of the snake continually moves forward, unable to stop, growing ever longer. It must be steered left, right, up, and down to avoid hitting walls and the body of either snake. The player who survives the longest wins.        
-            </div>
+        <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: '100vh' }}>
+        <div className="text-center">
+          <h1>Snake's rules </h1>
+          <p>
+            The "head" of the snake continually moves forward, unable to stop, growing ever longer. It must be steered left, right, up, and down to avoid hitting walls and the body of either snake. The player who survives the longest wins.
+          </p>
+        </div>
+      </div>
     )
 }
